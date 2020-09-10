3cb84b159cb0f8dea42a1dabfad2a3f05c575263
EdTellez <edtllz96@gmail.com>
2020-09-09 21:50:37 -0500

    Añadidos los archivos de lo mas basico de js

---
+++ curso_basico_js/funciones.js
@@ -0,0 +1,17 @@
+// Declarativas
+
+function miFuncion(){
+    return 3;
+}
+
+miFuncion();
+
+// Expresión
+// O funciones anonimas, porque se almacenan
+// dentro de una variable sin llevar nombre
+
+var miFuncion = function(a, b){
+  return a + b;
+}
+
+miFuncion();
---
+++ curso_basico_js/variables.js
@@ -0,0 +1,18 @@
+var nombre = "Eduardo";
+
+// declarar
+var edad;
+
+// inicializar
+edad = 30;
+
+edad
+
+var elementos = ["Computadora", "Celular"]
+
+var persona = {
+    nombre: "Eduardo",
+    edad: 23
+}
+
+persona;
